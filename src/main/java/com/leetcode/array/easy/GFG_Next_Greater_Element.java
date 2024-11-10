fec6c17c44dfc63fde7be9a2b43cfac7ead55d01
Murugesh Govindhappa <murugesh2311@gmail.com>
2024-11-11 00:23:04 +0530

    Stack LC

---
+++ src/main/java/com/leetcode/array/easy/GFG_Next_Greater_Element.java
@@ -0,0 +1,56 @@
+package com.leetcode.array.easy;
+
+import com.leetcode.Main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import java.util.Arrays;
+
+public class GFG_Next_Greater_Element {
+    public static void main(String[] args) {
+        GFG_Next_Greater_Element obj = new GFG_Next_Greater_Element();
+        int[] arr = new int[]{1, 3, 2, 4};
+
+        System.out.println(Arrays.toString(obj.nextLargerElement(arr)));
+    }
+    public int[] nextLargerElement(int[] arr) {
+        /*
+        Approach 1:
+        ArrayList<Integer> res = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {  // O(N)
+            int resEle = 0;
+            for (int j = i + 1; j < arr.length; j++) { // O(N-1)
+                if (arr[i] < arr[j]) {
+                    resEle = arr[j];
+                    break;
+                }
+            }
+            if (resEle == 0) {
+                resEle = -1;
+            }
+            res.add(resEle);
+        }
+        return res;
+        TC:O(N2)
+       */
+
+        /* Approach : using Stack
+        TC:O(N)
+        SC:O(N)
+        */
+        int[] res=new int[arr.length];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && stack.peek() < arr[i]) {
+                stack.pop();
+            }
+            res[i] = stack.isEmpty() ? -1 : stack.peek();
+
+            stack.push(arr[i]);
+        }
+        return res;
+    }
+}
