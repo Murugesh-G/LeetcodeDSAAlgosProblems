2a37a5fa5aece2b8928f849ad166416cb05219d5
Murugesh Govindhappa <murugesh2311@gmail.com>
2025-06-19 21:13:41 +0530

    in place reversal

---
+++ src/com/mgleetcode/stack/easy/LC496_NextGreaterElementI.java
@@ -0,0 +1,18 @@
+package com.mgleetcode.stack.easy;
+
+public class LC496_NextGreaterElementI {
+    public static void main(String[] args) {
+        LC496_NextGreaterElementI obj = new LC496_NextGreaterElementI();
+        int[] nums1 = new int[]{4, 1, 2};
+        int[] nums2 = new int[]{1, 3, 4, 2};
+        int[] res = obj.nextGreaterElement(nums1, nums2);
+        for (int n : res) {
+            System.out.print(n+" ");
+        }
+    }
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+
+    }
+
+}
